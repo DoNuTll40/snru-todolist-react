c067f4b8d154923ddcdbe17bac1f70c5efc27445
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-01-07 17:33:31 +0700

    Update : Bug UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="flex justify-between px-10 py-2">
         <h1 className="text-3xl font-bold text-pink-400">Homework ToDo List <FontAwesomeIcon icon={faListUl}/></h1>
       </div>
-      <div className="container mx-auto flex justify-center text-brown-text">
+      <div className="container ml-auto mr-auto max-w-[700px] p-4 text-brown-text">
         <TodoApp />
       </div>
     </>
